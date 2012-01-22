f1bb53aaa107a39b969e58cdb32e81839bd4b2b4
zeffii <tetha.z@gmail.com>
2012-01-22 11:52:41 +0000

    i want to keep these files seperate to show the idea

--- snippets/creating_custom_pattern_format_restructure.py
+++ snippets/creating_custom_pattern_format_restructure.py
@@ -9,7 +9,14 @@ LUNAR_SYNTH = "@trac.zeitherrschaft.org/aldrin/lunar/generator/synth;1"
 FUNKY_VERB = "@zzub.org/buzz2zzub/Larsha+Funkyverb"
 MATILDE_2 = "@zzub.org/buzz2zzub/Matilde+Tracker2"
 
-# we wrote this function earlier to take care of the machine loading and track count config
+
+def printInfo(format_name, num_gparams, num_tparams):
+    print("Created: " + format_name)
+    print("g_params: %d" % num_gparams)
+    print("t_params (params in note track) %d" % num_tparams)
+
+
+# take care of the machine loading and track count config
 def create_machine(uri, position, tracks, name):
   loader = player.get_pluginloader_by_name(uri)
   newplug = player.create_plugin(None, 0, name, loader)
@@ -17,6 +24,7 @@ def create_machine(uri, position, tracks, name):
   newplug.set_track_count(tracks)
   return newplug
 
+  
 # this handles the short notation "machine1 > machine2 > machine3" for chaining machines.
 def connect_machines(chain):
   machines = chain.split(" > ")
@@ -25,29 +33,6 @@ def connect_machines(chain):
     to_machine = player.get_plugin_by_name(machines[machine+1])
     to_machine.create_audio_connection(from_machine, 0, 2, 0, 2)
 
-
-matilde_1_location = (0, -1)
-matilde_2_location = (.25, -1)
-bassline_synth_location = (-.25, -1)
-verb_send_location = (.25, -.75)
-
-matilde1 = create_machine(MATILDE_2, matilde_1_location, 2, "Beats")
-matilde2 = create_machine(MATILDE_2, matilde_2_location, 3, "Snares")
-bassline_synth = create_machine(LUNAR_SYNTH, bassline_synth_location, 1, "Bassline")
-verb_effect = create_machine(FUNKY_VERB, verb_send_location, 0, "SnareFX")
-
-connect_machines("Beats > Master")
-connect_machines("Snares > SnareFX > Master")
-connect_machines("Bassline > Master")
-
-''' 
-up till this point we have now added the various machines and connected them as desired.
-- in create_machine we take care of the naming, location, machine type and track count 
-
-this following section will show a way to create a custom pattern format, because we aren't using the
-GUI we do manually have to add the default Pattern formats for each machine. no big deal, it will be part
-of the API soon.
-'''
 
 def count_format_columns(format):
     format_iterator = format.get_iterator()
@@ -98,19 +83,9 @@ def create_simple_format_from_machine(plugin):
     if num_tracks > 1:
         num_tracks_to_add = num_tracks - 1
         default_format = add_tracks_to_tparams(plugin, default_format, num_tracks_to_add)
-    
-    print("Created: " + format_name)
-    print("g_params: %d" % num_gparams)
-    print("t_params (params in note track) %d" % num_tparams)
+
+    printInfo(format_name, num_gparams, num_tparams)
     return default_format
-
-matilde1_format = create_simple_format_from_machine(matilde1)
-matilde2_format = create_simple_format_from_machine(matilde2)
-fxVerb_format = create_simple_format_from_machine(verb_effect)
-bassline_format = create_simple_format_from_machine(bassline_synth)
-
-# now we have the formats and can create a mixed_format
-mixed_format = player.create_pattern_format("MixFormat")
 
 
 def add_columns_to_format_from_plugin(format, plugin):
@@ -135,7 +110,30 @@ def add_subset_to_format_from_plugin(format, plugin, group, track, subset):
         format.add_column(plugin, group, track, column, column_idx)
         column_idx += 1
 
-# say i want   |bassline(all) | |beats(all) ! snare(all) |verb(wet)
+
+        
+matilde_1_location = (0, -1)
+matilde_2_location = (.25, -1)
+bassline_synth_location = (-.25, -1)
+verb_send_location = (.25, -.75)
+
+matilde1 = create_machine(MATILDE_2, matilde_1_location, 2, "Beats")
+matilde2 = create_machine(MATILDE_2, matilde_2_location, 3, "Snares")
+bassline_synth = create_machine(LUNAR_SYNTH, bassline_synth_location, 1, "Bassline")
+verb_effect = create_machine(FUNKY_VERB, verb_send_location, 0, "SnareFX")
+
+connect_machines("Beats > Master")
+connect_machines("Snares > SnareFX > Master")
+connect_machines("Bassline > Master")
+
+matilde1_format = create_simple_format_from_machine(matilde1)
+matilde2_format = create_simple_format_from_machine(matilde2)
+fxVerb_format = create_simple_format_from_machine(verb_effect)
+bassline_format = create_simple_format_from_machine(bassline_synth)
+
+# now we have the formats and can create a mixed_format
+# MixFormat = bassline(all) + beats(all) + snare(all) + verb(wet)
+mixed_format = player.create_pattern_format("MixFormat")
 column_idx = 0
 
 # bassline, beats, snare
@@ -145,8 +143,6 @@ add_columns_to_format_from_plugin(mixed_format, matilde2)
 # verb, one parameter wet, param 10 (index 9) in verb_effect plugin
 subset = [9]   # or something like ..[2,3,4,9]
 add_subset_to_format_from_plugin(mixed_format, verb_effect, 1, 0, subset)
-
-column_idx += 1
 
 
 player.history_commit(0, 0, "Added Formats")
